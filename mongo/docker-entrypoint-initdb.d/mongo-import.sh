60d1e2a8ac9a6713e5496c76c813d73fef2bf1d8
cdray527 <cdray085@hotmail.com>
2025-01-02 14:19:27 +0800

    Fix import script

--- mongo/docker-entrypoint-initdb.d/mongo-import.sh
+++ mongo/docker-entrypoint-initdb.d/mongo-import.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Check if the 'products' collection is empty
-if [ "$(mongosh --quiet --eval 'db.getSiblingDB("test").products.countDocuments()')" -eq "0" ]; then
+if [ "$(mongosh --quiet --eval 'db.getSiblingDB("nx-microray").products.countDocuments()')" -eq "0" ]; then
     echo "Initializing data in the 'products' collection..."
-    mongoimport --host localhost --db test --collection products --file /docker-entrypoint-initdb.d/products.json --jsonArray
+    mongoimport --host localhost --db nx-microray --collection products --file /docker-entrypoint-initdb.d/products.json --jsonArray
 else
     echo "'products' collection already initialized. Skipping data import."
 fi
